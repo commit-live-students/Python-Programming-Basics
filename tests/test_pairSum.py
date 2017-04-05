b4ee865881698c860b50a7a646f162f841625861
sangam <sangam.angre@gmail.com>
2017-04-05 11:51:56 +0530

    new module is added to handle all errors and exceptions

--- tests/test_pairSum.py
+++ tests/test_pairSum.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
-from pair_sum import pairSum
+from errors.exceptions import ImportException
 
 class TestPairSum(TestCase):
     def test_pairSum(self):
-        result = pairSum([2, 5, 4, 3, 8, 7, 6, 1, 10, -1], 9)
-        for item in result:
-            self.assertEqual(9, (item[0] + item[1]))
+        try:
+            from pair_sum import pairSum
+            result = pairSum([2, 5, 4, 3, 8, 7, 6, 1, 10, -1], 9)
+            for item in result:
+                self.assertEqual(9, (item[0] + item[1]))
+        except ImportError:
+            self.assertFalse(ImportException.no_function_found("pairSum"))
--- tests/test_squareOfNumbers.py
+++ tests/test_squareOfNumbers.py
@@ -1,14 +1,23 @@
 from unittest import TestCase
-from square_of_numbers import squareOfNumbers
+
+from errors.exceptions import ImportException
+
 
 class TestSquareOfNumbers(TestCase):
     def test_squareOfNumbers(self):
-        result = squareOfNumbers(8)
-        self.assertEqual(result[1], 1)
-        self.assertEqual(result[2], 4)
-        self.assertEqual(result[3], 9)
-        self.assertEqual(result[4], 16)
-        self.assertEqual(result[5], 25)
-        self.assertEqual(result[6], 36)
-        self.assertEqual(result[7], 49)
-        self.assertEqual(result[8], 64)
+        try:
+            from square_of_numbers import squareOfNumbers
+            result = squareOfNumbers(8)
+            if isinstance(result, dict):
+                self.assertEqual(result[1], 1)
+                self.assertEqual(result[2], 4)
+                self.assertEqual(result[3], 9)
+                self.assertEqual(result[4], 16)
+                self.assertEqual(result[5], 25)
+                self.assertEqual(result[6], 36)
+                self.assertEqual(result[7], 49)
+                self.assertEqual(result[8], 64)
+            else:
+                self.assertFalse("Function should return a dict object.")
+        except ImportError:
+            self.assertFalse(ImportException.no_function_found("squareOfNumbers"))
